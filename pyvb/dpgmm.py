089f06dd030e6edeef2158728d7ffb650b1cba77
du <du@du-OptiPlex-755.(none)>
2011-06-29 00:39:51 +0900

    dpgmm.py was added TODO :     add E_lnpi_StickBraking method to moments.py     add __init__, _init_prior, _init_posterior, getExpectations         _log_like_f, _KL_div, _update_parameters to dpgmm.py by inheritting from         VBGMM class

---
+++ pyvb/dpgmm.py
@@ -0,0 +1,10 @@
+﻿#!/usr/bin/python
+
+import numpy as np
+from util import logsum, log_like_Gauss2
+from sampling import testData
+from vbgmm import VBGMM
+from moments import *
+
+class DPGMM(VBGMM):
+    pass
