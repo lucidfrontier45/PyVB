5441b51f78871d1cac7893382ba575fb321ca3b1
lucidfrontier45 <lucidfrontier.45@gmail.com>
2011-08-25 18:40:57 +0900

    added viterbi_f to _hmmf.f90

--- pyvb/_hmmf.f90
+++ pyvb/_hmmf.f90
@@ -67,3 +67,37 @@ subroutine compute_lnEta_F(T,N,lnalpha,lnA,lnbeta,lnf,lnP_f,lneta)
         enddo
     enddo
 end subroutine
+
+subroutine viterbi_F(T,N,lnpi,lnA,lnf,z,lnP)
+    implicit none
+    integer, intent(in) :: T,N
+    real(8), intent(in) :: lnpi(N), lnA(N,N), lnf(T,N)
+    integer(8), intent(out) :: z(T)
+    real(8), intent(out) :: lnP
+    integer :: tt,i,j,imax(1)
+    real(8) :: lndelta(T,N)
+    real(8) :: temp(N)
+
+    ! Initialization
+    lndelta(1,:) = lnpi + lnf(1,:)
+
+    ! Induction
+    do tt = 2, T
+        do j = 1, N
+            temp(:) = lndelta(tt-1,:) + lnA(:,j)
+            lndelta(tt,j) = maxval(temp(:)) + lnf(tt,j)
+        enddo
+    enddo
+
+    lnP = maxval(lndelta(T,:))
+
+    ! Traceback
+    imax = maxloc(lndelta(T,:))
+    z(T) = imax(1)
+    
+    do tt = T-1, 1, -1
+        imax = maxloc(lndelta(tt,:) + lnA(:,z(tt+1)))
+        z(tt) = imax(1)
+    enddo
+
+end subroutine
